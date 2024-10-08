add38de1aded4473b298f49829057c6a61e60ea9
mpoplavkov <mihail.poplavkov@gmail.com>
2024-10-08 20:04:18 +0200

    useProvider logs

--- src/hooks/useProvider/useProvider.ts
+++ src/hooks/useProvider/useProvider.ts
@@ -3,9 +3,19 @@ import {NetworkUrl} from "@/src/utils/constants";
 import {useQuery} from "@tanstack/react-query";
 
 const useProvider = () => {
-  const { data } = useQuery({
+  const {data} = useQuery({
     queryKey: ['provider'],
-    queryFn: () => Provider.create(NetworkUrl),
+    queryFn: async () => {
+      console.log("Creating provider with network url", NetworkUrl);
+      try {
+        const provider = await Provider.create(NetworkUrl)
+        console.log("Created provider", provider.url);
+        return provider;
+      } catch (e) {
+        console.error("Error creating provider", e);
+        throw e;
+      }
+    },
   });
 
   return data;
